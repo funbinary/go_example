ade195a237334be4cbd8f1308413c9232dbe25d3
funbinary <17805984441@163.com>
2023-04-12 20:30:28 +0800

    feat: crawler学习

---
+++ example/crawler/14/collect/collect.go
@@ -0,0 +1,91 @@
+package collect
+
+import (
+	"bufio"
+	"github.com/funbinary/go_example/example/crawler/14/proxy"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io"
+	"net/http"
+	"time"
+)
+
+type Fetcher interface {
+	Get(url *Request) ([]byte, error)
+}
+
+type BaseFetch struct {
+}
+
+func (b *BaseFetch) Get(req *Request) ([]byte, error) {
+
+	resp, err := http.Get(req.Url)
+	if err != nil {
+		return nil, errors.Wrap(err, "fetch url error")
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.Errorf("Error status code:%v", resp.StatusCode)
+	}
+
+	r := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(r)
+	utf8r := transform.NewReader(r, e.NewDecoder())
+	return io.ReadAll(utf8r)
+}
+
+type BrowserFetch struct {
+	Timeout time.Duration
+	Proxy   proxy.ProxyFunc
+	Logger  *zap.Logger
+}
+
+func (b *BrowserFetch) Get(request *Request) ([]byte, error) {
+	client := &http.Client{
+		Timeout: b.Timeout,
+	}
+
+	if b.Proxy != nil {
+		transport := http.DefaultTransport.(*http.Transport)
+		transport.Proxy = b.Proxy
+		client.Transport = transport
+	}
+
+	req, err := http.NewRequest("GET", request.Url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
+	//req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	time.Sleep(request.WaitTime)
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.Errorf("Error status code:%v", resp.StatusCode)
+	}
+
+	r := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(r)
+	utf8r := transform.NewReader(r, e.NewDecoder())
+	return io.ReadAll(utf8r)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
